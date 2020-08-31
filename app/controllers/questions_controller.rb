11ee61d50d17679024d599e1475a8225402d535b
Mathieu Longe <mathieu.longe@orange.fr>
2020-08-31 14:58:16 +1000

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,11 @@
 class QuestionsController < ApplicationController
-  def ask
-    params[:question]
-  end
+  def ask; end
 
   def answer
-    if params[:question] == "I am going to work"
+    @question = params[:question]
+    if @question == "I am going to work"
       @answer = "Great"
-    elsif params[:question].last == "?"
+    elsif @question.last == "?"
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
